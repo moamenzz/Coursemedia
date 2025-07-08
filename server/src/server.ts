c42a91aa76e4b8f7eb196ac5a90fa0b0dfb77c57
Moamen Muhammed <a5tsasyalcomputer@gmail.com>
2025-07-08 22:43:10 +0300

    Readme.md cover

--- server/src/server.ts
+++ server/src/server.ts
@@ -62,10 +62,6 @@ app.use("/purchase", authenticate, purchaseRouter);
 app.use("/player", authenticate, playerRouter);
 app.use("/review", reviewRouter);
 
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My Third Sentry Error!");
-});
-
 Sentry.setupExpressErrorHandler(app);
 
 app.use(errorHandler);
